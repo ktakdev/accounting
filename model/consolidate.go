b2df40646c3927aa08e52f9ba5641190c5905102
ktakdev <ktak.dev@gmail.com>
2024-12-16 00:48:04 +0900

    PLも連結するように

--- model/consolidate.go
+++ model/consolidate.go
@@ -1,11 +1,20 @@
 package model
 
-// BSの連結
-func Consolidate(primary, subsidiary BalanceSheet) BalanceSheet {
-	return BalanceSheet{
+// BS, PLの連結
+func Consolidate(primaryBS, subsidiaryBS BS, primaryPL, subsidiaryPL PL) (BS, PL) {
+	consolidatedBS := BS{
+		OtherAssets:      primaryBS.OtherAssets + subsidiaryBS.OtherAssets,
 		SubsidiaryStock:  0,
-		Capital:          primary.Capital + subsidiary.Capital,
-		CapitalSurplus:   primary.CapitalSurplus,
-		RetainedEarnings: primary.RetainedEarnings,
+		OtherLiabilities: primaryBS.OtherLiabilities + subsidiaryBS.OtherLiabilities,
+		Capital:          primaryBS.Capital,
+		CapitalSurplus:   primaryBS.CapitalSurplus,
+		RetainedEarnings: primaryBS.RetainedEarnings + primaryPL.NetIncome + subsidiaryPL.NetIncome,
 	}
+	consolidatedPL := PL{
+		NetIncome:     primaryPL.NetIncome + subsidiaryPL.NetIncome,
+		OtherExpenses: primaryPL.OtherExpenses + subsidiaryPL.OtherExpenses,
+		OtherIncome:   primaryPL.OtherIncome + subsidiaryPL.OtherIncome,
+	}
+
+	return consolidatedBS, consolidatedPL
 }
