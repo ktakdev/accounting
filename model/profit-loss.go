5b756683f78886e7340dd24406409834dfbc6fa5
ktakdev <ktak.dev@gmail.com>
2024-12-16 01:08:07 +0900

    profit-lossに名称変更

--- model/profit-loss.go
+++ model/profit-loss.go
@@ -1,9 +1,9 @@
 package model
 
-type ProfitAndLossStatement struct {
+type ProfitLoss struct {
 	OtherExpenses int `json:"諸費用"`
 	NetIncome     int `json:"当期純利益"`
 	OtherIncome   int `json:"諸収益"`
 }
 
-type PL = ProfitAndLossStatement
+type PL = ProfitLoss
