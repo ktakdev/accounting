582d63c6dcbbc9121fde6aaeacb83caaf1c61b62
ktakdev <ktak.dev@gmail.com>
2024-12-15 23:53:40 +0900

    impl conslidate to bs

--- model/balance-sheet.go
+++ model/balance-sheet.go
@@ -3,3 +3,10 @@ package model
 type BalanceSheet struct {
 	Capital int `json:"capital"`
 }
+
+// BSの連結
+func Consolidate(primary, subsidiary BalanceSheet) BalanceSheet {
+	return BalanceSheet{
+		Capital: primary.Capital + subsidiary.Capital,
+	}
+}
