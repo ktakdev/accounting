0b21be2fdca2d2ca470a3f9e5afc1ef395deeade
ktakdev <ktak.dev@gmail.com>
2024-12-16 00:32:09 +0900

    その他項目の追加

--- model/profit-and-loss-statement.go
+++ model/profit-and-loss-statement.go
@@ -1,7 +1,9 @@
 package model
 
 type ProfitAndLossStatement struct {
-	NetIncome int `json:"当期純利益"`
+	NetIncome     int `json:"当期純利益"`
+	OtherExpenses int `json:"諸費用"`
+	OtherIncome   int `json:"諸収益"`
 }
 
 type PL = ProfitAndLossStatement
