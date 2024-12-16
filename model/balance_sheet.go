10ae8a57165f6bd5096604bdfc2a5da1f8818362
ktakdev <ktak.dev@gmail.com>
2024-12-16 14:19:26 +0900

    コードの配置変更

--- model/balance_sheet.go
+++ model/balance_sheet.go
@@ -1,5 +1,9 @@
 package model
 
+type BalanceSheet struct {
+	Debit  BSDebit  `json:"借方"`
+	Credit BSCredit `json:"貸方"`
+}
 type BSDebit struct {
 	OtherAssets     int `json:"諸資産"`
 	SubsidiaryStock int `json:"子会社株式"`
@@ -10,11 +14,6 @@ type BSCredit struct {
 	Capital          int `json:"資本金"`
 	CapitalSurplus   int `json:"資本剰余金"`
 	RetainedEarnings int `json:"利益剰余金"`
-}
-
-type BalanceSheet struct {
-	Debit  BSDebit  `json:"借方"`
-	Credit BSCredit `json:"貸方"`
 }
 
 type BS = BalanceSheet
